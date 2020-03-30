3883984c4d28d4b08b52bac36fcc7a0187a2bc21
steelburrito <antouniokevin@gmail.com>
2020-03-30 11:32:59 +0700

    Now able to read url path variables and query string request params.

--- src/main/java/com/example/mobileapprestful/ui/controller/UserController.java
+++ src/main/java/com/example/mobileapprestful/ui/controller/UserController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users") //http:localhost:8080/users
 public class UserController {
     @GetMapping
-    public String getUser() {
-        return "user is called";
+    public String getUsers(@RequestParam(value = "page") int page, @RequestParam(value = "limit") int limit) {
+        return "Users are called from page " + page + " with limit " + limit;
+    }
+
+    @GetMapping(path = "/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "User " + userId + " is called";
     }
 
     @PostMapping
