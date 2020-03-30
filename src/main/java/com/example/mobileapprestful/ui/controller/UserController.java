11b259f93794de77ac50fd7f168e58c2809cd2a2
steelburrito <antouniokevin@gmail.com>
2020-03-30 13:05:20 +0700

    added more query string params, params are now optional

--- src/main/java/com/example/mobileapprestful/ui/controller/UserController.java
+++ src/main/java/com/example/mobileapprestful/ui/controller/UserController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users") //http:localhost:8080/users
 public class UserController {
     @GetMapping
-    public String getUsers(@RequestParam(value = "page") int page, @RequestParam(value = "limit") int limit) {
-        return "Users are called from page " + page + " with limit " + limit;
+    //default value will make page parameter optional because a value will be assigned if there is none
+    //a default value must be set due to page and limit declared as a primitive type
+    public String getUsers(@RequestParam(value = "page", defaultValue = "111", required = false) int page,
+                           @RequestParam(value = "limit", defaultValue = "10") int limit,
+                           @RequestParam(value = "sortBy", defaultValue = "default", required = false) String sortBy) {
+        return "Users are called from page " + page + " with limit " + limit + " and sort by " + sortBy;
     }
 
     @GetMapping(path = "/{userId}")
